31512f2b4fce6c3e6d0034d45455e204c9a7e291
Pzqqt <821026875@qq.com>
2024-09-11 11:20:15 +0800

    AK3: No need to unpack the ramdisk of the boot partition image
    - Drop {boot,vendor_boot}_attributes() as well.
    
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -19,11 +19,6 @@ supported.vendorpatchlevels=
 
 
 ### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
 
 # boot shell variables
 BLOCK=boot;
@@ -35,16 +30,10 @@ PATCH_VBMETA_FLAG=auto;
 . tools/ak3-core.sh;
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
-
-## vendor_boot files attributes
-vendor_boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
 
 # vendor_boot shell variables
 BLOCK=vendor_boot;
