f03d7993ad85ccd506dc5e828d3f7090cc216553
Cyber Knight <cyberknight755@gmail.com>
2025-06-18 05:08:54 +0800

    AK3: Support updating vendor_dlkm and vendor_ramdisk modules
    - This implementation updates modules in vendor_dlkm and vendor_ramdisk in a seamless manner.
    - It was done to be able to iterate with quick changes while using modules without requiring the AOSP build system to build vendor_dlkm or vendor_boot.
    - It requires the following hierarchy of files:
      - modules/dlkm.tar.xz
        - lib/modules/*.ko
        - lib/modules/modules.* (generated by depmod and a modules.load list)
      - modules/dlkm.cpio.lz4
        - lib/modules/*.ko
        - lib/modules/modules.* (generated by depmod and a modules.load list)
      - config/vendor_dlkm_file_contexts (generated to change SELinux context for the vendor_dlkm partition)
      - config/vendor_dlkm_fs_config (generated to change permissions for the vendor_dlkm partition)
    
    - It also requires these tools:
      - extract.erofs (required to extract vendor_dlkm image)
      - mkfs.erofs (required to generate a new vendor_dlkm image)
    
    - Moreover, it also requires vendor_boot to have a separate dlkm vendor_ramdisk fragment specific to modules.
    - This was highly inspired by [1].
    
    [1]: https://github.com/Pzqqt/AnyKernel3/blob/Marble-Melt/anykernel.sh
    
    Co-authored-by: Pzqqt <821026875@qq.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -58,6 +58,79 @@ reset_ak;
 # vendor_boot install
 split_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 
+if [ -f $AKHOME/modules/dlkm.cpio.lz4 ]; then
+    ui_print " " "- [✓] LZ4 CPIO archive found. Starting vendor_ramdisk modules update..."
+
+    ui_print "- [•] Decompressing the archive..."
+    magiskboot decompress $AKHOME/modules/dlkm.cpio.lz4 $AKHOME/dlkm.cpio || \
+        abort "[✗] Failed to decompress LZ4 CPIO archive"
+
+    magiskboot cpio $AKHOME/dlkm.cpio test
+    cpio_check=$?
+    if [ $cpio_check -eq 1 ]; then
+        abort "[✗] Decompressed CPIO archive error"
+    fi
+
+    ui_print "- [•] Updating vendor_ramdisk modules..."
+    mv $AKHOME/dlkm.cpio $SPLITIMG/vendor_ramdisk/dlkm.cpio || \
+        abort "[✗] Updating vendor_ramdisk modules failed"
+
+    ui_print "- [✓] Flashing new vendor_ramdisk image..."
+fi
+
 flash_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
 
+
+# reset for vendor_dlkm patching
+reset_ak;
+
+## vendor_dlkm install
+if [ -f $AKHOME/modules/dlkm.tar.xz ]; then
+    ui_print " " "/dev/block/mapper/vendor_dlkm${SLOT}"
+    ui_print " " "- [✓] XZ tarball found. Starting /vendor_dlkm modules update..."
+
+    ui_print "- [•] Pulling /vendor_dlkm image from current slot (${SLOT})..."
+    dd if=/dev/block/mapper/vendor_dlkm${SLOT} of=${AKHOME}/vendor_dlkm.img || \
+        abort "[✗] Failed to pull vendor_dlkm${SLOT}.img"
+    extract_vendor_dlkm_dir=${AKHOME}/_extract_vendor_dlkm
+    mkdir -p $extract_vendor_dlkm_dir || \
+        abort "[✗] Failed to create $extract_vendor_dlkm_dir"
+
+    ui_print "- [•] Unpacking /vendor_dlkm image..."
+    ${BIN}/extract.erofs -i ${AKHOME}/vendor_dlkm.img -x -T8 -o ${extract_vendor_dlkm_dir} &> /dev/null || \
+        abort "[✗] Failed to unpack the vendor_dlkm image"
+    sync
+
+    ui_print "- [•] Updating /vendor_dlkm modules..."
+    extract_vendor_dlkm_modules_dir=${extract_vendor_dlkm_dir}/vendor_dlkm/lib/modules
+    rm -f ${extract_vendor_dlkm_modules_dir}/* || \
+        abort "[✗] Failed to remove pre-existing files in ${extract_vendor_dlkm_modules_dir}"
+    rm -f ${extract_vendor_dlkm_dir}/config/vendor_dlkm_{fs_config,file_contexts} || \
+        abort "[✗] Failed to remove pre-existing fs_config and file_contexts in ${extract_vendor_dlkm_dir}/config"
+    busybox tar -xpf ${AKHOME}/modules/dlkm.tar.xz -C ${extract_vendor_dlkm_dir}/vendor_dlkm/ || \
+        abort "[✗] Failed to extract XZ-compressed tarball"
+    mv ${AKHOME}/config/vendor_dlkm* ${extract_vendor_dlkm_dir}/config/ || \
+        abort "[✗] Failed to move fs_config and file_contexts to ${extract_vendor_dlkm_dir}/config"
+
+    ui_print "- [•] Repacking /vendor_dlkm image..."
+    rm -f ${AKHOME}/vendor_dlkm.img || \
+        abort "[✗] Failed to remove pre-existing vendor_dlkm.img"
+    ${BIN}/mkfs.erofs \
+          --mount-point /vendor_dlkm \
+          --fs-config-file ${extract_vendor_dlkm_dir}/config/vendor_dlkm_fs_config \
+          --file-contexts ${extract_vendor_dlkm_dir}/config/vendor_dlkm_file_contexts \
+          -z lz4hc,9 \
+          -b 4096 \
+          -C 262144 \
+          -T 1230768000 \
+          ${AKHOME}/vendor_dlkm.img ${extract_vendor_dlkm_dir}/vendor_dlkm || \
+        abort "[✗] Failed to repack the vendor_dlkm image"
+    rm -rf ${extract_vendor_dlkm_dir} || \
+        abort "[✗] Failed to remove working directory"
+    unset extract_vendor_dlkm_dir extract_vendor_dlkm_modules_dir
+
+    ui_print "- [✓] Flashing new /vendor_dlkm image..."
+    flash_generic vendor_dlkm;
+fi
+## end vendor_dlkm install
